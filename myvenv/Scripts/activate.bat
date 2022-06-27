81d4d7668dd7efa6aca2342a2e56698d42d160ed
unknown <yumiiiiiii>
2022-06-28 03:19:43 +0900

    codelion_movieproject

--- myvenv/Scripts/activate.bat
+++ myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jain5\Desktop\codelion_snsproject\myvenv
+set VIRTUAL_ENV=C:\Users\jain5\Desktop\codelion_movieproject\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
